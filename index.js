a9e28f74c7b7a43b93a9388f907adee54685e064
Alex Manzi <alex.manzi22@gmail.com>
2022-02-16 16:14:43 -0500

    wednesday afternoon

--- index.js
+++ index.js
@@ -15,44 +15,60 @@ function fetchTeam() {
     })
 )}
 
-    let mainArea = document.querySelector('#imageholder')
-
-
-
 function listEastTeams(team) {
     if (team.conference === "East") {
         let eastSide = document.querySelector('#eastern-teams')
+        let mainArea = document.querySelector('#imageholder2')
+        let eastTeam = document.querySelector("#Value2")
+        let teamRatingGame = (Math.random() * 3) * team.score
+        
         let listOfEast = document.createElement('h2')
-       
         listOfEast.textContent = team.full_name
-       
         listOfEast.addEventListener('click', (e) => {
             e.preventDefault()
             mainArea.src = team.image
+            eastTeam.textContent = teamRatingGame
+            console.log(teamRatingGame)
         })
         
-        
-
         eastSide.appendChild(listOfEast) 
-
     }
 }
 
 function listWestTeams(team) {
     if (team.conference === 'West') {
         let westSide = document.querySelector('#western-teams')
-        let listOfWest = document.createElement('h2')
+        let mainArea = document.querySelector('#imageholder')
+        let westTeam = document.querySelector("#Value1")
+        let teamRatingGame = (Math.random() * 5) * team.score
         
+        let listOfWest = document.createElement('h2')
         listOfWest.textContent = team.full_name
-
         listOfWest.addEventListener('click', (e) => {
             e.preventDefault()
             mainArea.src = team.image
+            westTeam.textContent = teamRatingGame
+            console.log(teamRatingGame)
         })
 
         westSide.appendChild(listOfWest)
     }
 }
+
+function playGame() {
+    let goGame = document.querySelector('#simGame')
+    let westValue = document.querySelector('#Value1').value
+    let eastValue = document.querySelector('#Value2').value
+    goGame.addEventListener('click', () => {
+        if (eastValue > westValue) {
+            console.log('The East Wins')
+        } else {
+            console.log('The West Wins')
+    }
+    })
+}
+    
+playGame()
 
 //standings are as of Feb 16 - 2022
 function addJersey(team) {
@@ -85,7 +101,7 @@ function addJersey(team) {
         team.score = .586
     }
     else if (team.id === 8) {
-        team.image = "https://logos-world.net/wp-content/uploads/2020/05/Denver-Nuggets-Logo.png"
+        team.image = "https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Denver_Nuggets.svg/800px-Denver_Nuggets.svg.png"
         team.score = .561
     }
     else if (team.id === 9) {
@@ -93,11 +109,11 @@ function addJersey(team) {
         team.score = .211
     }
     else if (team.id === 10) {
-        team.image = "https://logos-world.net/wp-content/uploads/2020/05/Golden-State-Warriors-logo.png"
+        team.image = "https://upload.wikimedia.org/wikipedia/en/thumb/0/01/Golden_State_Warriors_logo.svg/1200px-Golden_State_Warriors_logo.svg.png"
         team.score = .724
     }
     else if (team.id === 11) {
-        team.image = "https://logos-world.net/wp-content/uploads/2020/05/Houston-Rockets-logo.png"
+        team.image = "https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Houston_Rockets.svg/800px-Houston_Rockets.svg.png"
         team.score = .268
     }
     else if (team.id === 12) {
@@ -105,15 +121,15 @@ function addJersey(team) {
         team.score = .322
     }
     else if (team.id === 13) {
-        team.image = "https://logos-world.net/wp-content/uploads/2020/05/Los-Angeles-Clippers-Logo-1985-2010.png"
+        team.image = "https://i.pinimg.com/564x/9e/1c/4a/9e1c4a56869d0f0c2d4f3f017bf4df79.jpg"
         team.score = .483
     }
     else if (team.id === 14) {
-        team.image = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Los_Angeles_Lakers_logo.svg/1200px-Los_Angeles_Lakers_logo.svg.png"
+        team.image = "pictures/lakers.png"
         team.score = .456
     }
     else if (team.id === 15) {
-        team.image = "https://logos-world.net/wp-content/uploads/2020/05/Memphis-Grizzlies-Logo-2005-2018.png"
+        team.image = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/Memphis_Grizzlies.svg/1200px-Memphis_Grizzlies.svg.png"
         team.score = .695
     }
     else if (team.id === 16) {
@@ -125,11 +141,11 @@ function addJersey(team) {
         team.score = .610
     }
     else if (team.id === 18) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2017/08/Minnesota-Timberwolves-Logo-2008.jpg'
+        team.image = 'https://cdn.freebiesupply.com/images/large/2x/minnesota-timberwolves-logo-transparent.png'
         team.score = .534
     }
     else if (team.id === 19) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2017/04/New-Orleans-Saints-Logo-2000.jpg'
+        team.image = 'https://www.nba.com/.element/img/1.0/teamsites/logos/teamlogos_500x500/nop.png'
         team.score = .397
     }
     else if (team.id === 20) {
@@ -137,7 +153,7 @@ function addJersey(team) {
         team.score = .431
     }
     else if (team.id === 21) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2018/06/Oklahoma-City-Thunder-Logo.png'
+        team.image = 'https://cdn.freebiesupply.com/images/large/2x/oklahoma-city-thunder-logo-transparent.png'
         team.score = .316
     }
     else if (team.id === 22) {
@@ -149,19 +165,19 @@ function addJersey(team) {
         team.score = .596
     }
     else if (team.id === 24) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2018/06/Phoenix-Suns-Logo-1968.jpg'
+        team.image = 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/Phoenix_Suns_logo.svg/1200px-Phoenix_Suns_logo.svg.png'
         team.score = .825
     }
     else if (team.id === 25) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2018/06/Portland-Trail-Blazers-logo-1.jpg'
+        team.image = 'https://upload.wikimedia.org/wikipedia/sco/thumb/7/74/Portland_Trail_Blazers.svg/1200px-Portland_Trail_Blazers.svg.png'
         team.score = .414
     }
     else if (team.id === 26) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2018/06/Los-Angeles-Kings-Logo-1975.png'
+        team.image = 'https://cdn.freebiesupply.com/images/large/2x/sacramento-kings-logo-transparent.png'
         team.score = .373
     }
     else if (team.id === 27) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2018/01/San-Antonio-Spurs-Logo-2002.png'
+        team.image = 'http://tsnimages.tsn.ca/ImageProvider/TeamLogo?seoId=san-antonio-spurs&width=620&height=620'
         team.score = .379
     }
     else if (team.id === 28) {
@@ -169,14 +185,14 @@ function addJersey(team) {
         team.score = .554
     }
     else if (team.id === 29) {
-        team.image = 'https://1000logos.net/wp-content/uploads/2018/05/Utah_Jazz_Logo.png'
+        team.image = 'https://scoutanalytica.com/wp-content/uploads/2018/09/uta.png'
         team.score = .632
     }
     else if (team.id === 30) {
         team.image = 'https://1000logos.net/wp-content/uploads/2018/05/Washington_Wizards_Logo.png'
         team.score = .464
     }
-    console.log(team)
+    //console.log(team)
 }
 
 
